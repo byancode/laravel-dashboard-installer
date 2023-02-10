7f5f2be5ad5541166a71dd3e2dbea9edbf5af719
Byancode <byancode@gmail.com>
2023-02-09 21:10:13 -0500

    set MYSQL_ATTR_SSL_CA

--- src/Controllers/InstallDatabaseController.php
+++ src/Controllers/InstallDatabaseController.php
@@ -53,6 +53,9 @@ class InstallDatabaseController extends Controller
                         'username' => $request->input('database_username'),
                         'password' => $request->input('database_password'),
                         'prefix' => $request->input('database_prefix'),
+                        'options' => [
+                            \PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                        ],
                     ]),
                 ],
             ],
